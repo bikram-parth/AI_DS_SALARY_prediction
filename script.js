3ca2ae64f2f143bb246cdabfa6ebd036952dd45a
bikram-parth <bikramdebnath.2006@gmail.com>
2026-09-18 13:29:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://ai-ds-salary-prediction-1.onrender.com/predict";
 
 const form = document.getElementById("predictForm");
 const submitBtn = document.getElementById("submitBtn");
